0d6e6f587a0afeffa1dee1406cdb4a579611ddc6
Baylen <baylendoss12@gmail.com>
2021-01-07 12:40:56 -0600

    your turn

--- storage-project.js
+++ storage-project.js
@@ -1,5 +1,13 @@
 window.addEventListener("DOMContentLoaded", (event) => {
+  const cartButton = document.getElementById("add-to-cart");
+  let itemCount = 0;
+  cartButton.addEventListener("click", event => {
+    event.preventDefault()
+    itemCount++
+    let select = document.getElementById("items");
+    localStorage.setItem(`item${itemCount}`, select.value);
 
+  })
   // const showCart = () => {
 
   // };
